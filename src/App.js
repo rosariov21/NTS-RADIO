77e08f20cfdef3b176e9dad04a85cdf5a998683c
rosariov21 <rosariovelazquez@Rosarios-MBP.home>
2020-09-01 17:05:42 -0400

    more

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>NTS RADIO </h1> 
+
+     <figure>
+            <figcaption>Listen</figcaption>
+            <audio
+                controls
+                src="./media/audio-file1.mp3">
+                    Your browser does not support the
+                    <code>audio</code> element.
+            </audio>
+        </figure>
+       
+
+
+
+
     </div>
   );
 }
