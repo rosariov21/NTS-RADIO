6a5740969f7bcaf42b5ccb3d9c77c5ab4afb1aa8
rosariov21 <rosariovelazquez@Rosarios-MBP.home>
2020-09-04 16:03:15 -0400

    more

---
+++ src/Components/NavBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const NavBar = () => {
+    return(
+    <nav>
+      <ul>
+    <li className="Parent_nav">
+        NTS
+    </li>
+      <li className="navChild">
+      Radio
+      </li>
+      
+      <li className="nav">
+      Latest
+      </li>
+
+      <li className="nav">
+      Explore
+      </li>
+
+      <li className="nav">
+      Infinite Mixtapes
+      </li>
+    </ul>
+    </nav>
+    );
+}
+
+export default NavBar;
